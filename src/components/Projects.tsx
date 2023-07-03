5065417e9b1b67d4c9d2d7e1b5a33431cda87a48
Renan Andrade <fel9.renan02@gmail.com>
2023-07-03 20:17:31 -0300

    Added landing page project

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,5 +1,6 @@
 import { SectionTitle } from "./SectionTitle";
 import { ProjectsList } from "./ProjectsList";
+import { ProjectsInfo } from "@/data/ProjectsInfo";
 
 interface ProjectInfo {
   title: string;
@@ -12,36 +13,7 @@ interface ProjectInfo {
 }
 
 export const Projects = () => {
-  const projectInfoList: ProjectInfo[] = [
-    {
-      title: "Full stack app",
-      description:
-        "The project combined Twitch&apos;s API with ExpressJs and NextJs for a responsive SPA. Users can create and view gaming ads using Discord for login. The server, built with Expressjs, offers data fetching and ad creation endpoints. TypeScript ensures code safety.",
-      skills: [
-        "NextJS",
-        "Tailwind",
-        "ExpressJS",
-        "Next-auth",
-        "Typescript",
-        "Vercel",
-      ],
-      imgUrl: "/nlw-web.png",
-      projectType: "Featured Project",
-      githubLink: "https://github.com/RenanFelipeAndrade/NLW-Web",
-      finalUrl: "https://nlw-web-phi.vercel.app/",
-    },
-    {
-      title: "Ecommerce",
-      description:
-        "This project was created using Next.js, TypeScript, and Sanity. It is fully responsive for desktops, tablets, and smartphones. The interface was developed with NextJS and TypeScript, styled using vanilla CSS. Sanity is integrated for product and content management. It also has a Stripe integration in the checkout process.",
-      skills: ["NextJS", "Sanity", "Stripe", "Typescript", "Vercel"],
-      imgUrl: "/ecommerce.png",
-      projectType: "Featured Project",
-      githubLink: "https://github.com/RenanFelipeAndrade/ecommerce",
-      finalUrl: "https://ecommerce-sanity-stripe-hazel.vercel.app/",
-    },
-  ];
-
+  const projectInfoList: ProjectInfo[] = ProjectsInfo;
   return (
     <section
       id="projects"
---
+++ src/data/ProjectsInfo.ts
@@ -0,0 +1,39 @@
+export const ProjectsInfo = [
+  {
+    title: "Full stack app",
+    description:
+      "The project combined Twitch&apos;s API with ExpressJs and NextJs for a responsive SPA. Users can create and view gaming ads using Discord for login. The server, built with Expressjs, offers data fetching and ad creation endpoints. TypeScript ensures code safety.",
+    skills: [
+      "NextJS",
+      "Tailwind",
+      "ExpressJS",
+      "Next-auth",
+      "Typescript",
+      "Vercel",
+    ],
+    imgUrl: "/nlw-web.png",
+    projectType: "Featured Project",
+    githubLink: "https://github.com/RenanFelipeAndrade/NLW-Web",
+    finalUrl: "https://nlw-web-phi.vercel.app/",
+  },
+  {
+    title: "Landing page",
+    description:
+      "This project was created using Next.js, TypeScript and Framer Motion. Based on a public design, this project is a fitness-related landing page with a lot of creative changes aiming to improve the original design.",
+    skills: ["NextJS", "Tailwind", "Framer Motion", "Typescript", "Vercel"],
+    imgUrl: "/fitsoft.png",
+    projectType: "Featured Project",
+    githubLink: "https://github.com/RenanFelipeAndrade/landing-page",
+    finalUrl: "https://fitsoft-landing-page.vercel.app/",
+  },
+  {
+    title: "Ecommerce",
+    description:
+      "This project was created using Next.js, TypeScript, and Sanity. It is fully responsive for desktops, tablets, and smartphones. The interface was developed with NextJS and TypeScript, styled using vanilla CSS. Sanity is integrated for product and content management. It also has a Stripe integration in the checkout process.",
+    skills: ["NextJS", "Sanity", "Stripe", "Typescript", "Vercel"],
+    imgUrl: "/ecommerce.png",
+    projectType: "Featured Project",
+    githubLink: "https://github.com/RenanFelipeAndrade/ecommerce",
+    finalUrl: "https://ecommerce-sanity-stripe-hazel.vercel.app/",
+  },
+];
